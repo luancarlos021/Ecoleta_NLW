4c618b60b66119bd8aaccf14894181baea8bae30
Luan Carlos <luancarlos021@gmail.com>
2020-06-04 18:36:09 -0300

    Arquivo Js para popular o estado e o município, coletando dados da api do IBGE.

---
+++ scripts/create-point.js
@@ -0,0 +1,61 @@
+// console.log("Hello")
+// https://servicodados.ibge.gov.br/api/v1/localidades/estados
+
+// fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome").then(function(res){return res.json() }).then(function(data){console.log(data)})
+// https://servicodados.ibge.gov.br/api/v1/localidades/estados/{uf}/municipios?orderBy=nome
+
+function get_uf_cities(url,select) {
+    fetch(url)
+    .then( res => res.json() )
+    .then( data => {
+        for ( const value of data ){
+            select.innerHTML += `<option value="${value.id}">${value.nome}</option>`
+        }
+    }
+    )
+}
+
+function populateUFs() {
+    const ufSelect = document.querySelector("select[name=uf")
+
+    const url_uf = "https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome"
+    get_uf_cities(url_uf, ufSelect)
+
+    // fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome")
+    // .then( res => res.json() )
+    // .then( states => {
+        
+    //     for ( const state of states ){
+    //         ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`            
+    //     }
+    // })
+}
+
+populateUFs()
+
+function getCities(event) {
+    const citySelect = document.querySelector("select[name=city]")
+    const stateInput = document.querySelector("input[name=state]")
+
+    const id_uf = event.target.value
+    console.log(id_uf)
+
+    stateInput.value = event.target.options[event.target.selectedIndex].text
+
+    const url_city = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${id_uf}/municipios?orderBy=nome`
+
+
+    citySelect.innerHTML = '<option value="">Selecione a cidade</option>'
+    if (id_uf == "") {
+        console.log("entrei")
+        citySelect.disabled = true
+    } else {
+        citySelect.disabled = false
+    }
+
+    get_uf_cities(url_city, citySelect)
+}
+
+document
+    .querySelector("select[name=uf]")
+    .addEventListener("change", getCities)
