69954c7f3dc1b8920d5a6c4554dcf2bb6b862310
Luan Carlos <luancarlos021@gmail.com>
2020-06-04 18:39:10 -0300

    Introdução ao JS

---
+++ scripts/intro.js
@@ -0,0 +1,76 @@
+// comments
+// document.write("Hello")
+
+// variaveis, tipos de dados
+// var myvar = "Ha"
+// let mylet = "He"
+// const myconst = "Hi"
+
+// document.write(myconst + myvar + mylet)
+
+
+// string
+// const s1 = "Isso é uma string"
+// const s2 = 'Isso também é uma string'
+// const s3 = `Isso é uma string também`
+
+// number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// Estrutura de Dados
+
+// objetos possuem propriedades e funcionalidades
+
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: false,
+//   correr(){
+//     document.write("executar uma grande logica de correr")
+//   }
+// }
+
+// pessoa.correr()
+// document.write(pessoa.correr())
+
+// Array - Vetores  -- Coleção de dados
+ 
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "My Name"}]
+// document.write(colecaoDeBolinhas[3].name)
+
+
+// Funções - Funcionalidades - Atalhos - Reuso de código
+
+// registrar função
+// function sayMyName(name) {
+//   document.write(name)
+// }
+
+// // executar função
+// sayMyName("Luan")
+// sayMyName("Jose")
+// sayMyName("Rapadura")
+
+
+// condicionais
+
+// const notaFinal = 7
+
+// if( notaFinal < 5 ) {
+//   document.write("Reprovado")
+// } else {
+//   document.write("Aprovado")
+// }
+
+// loop repetições
+
+// for (i = 0; i <= 10; i++) {
+//   document.write(`<p> ${i} </p>`)
+// }
