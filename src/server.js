411463dbfa0ef51f8fec28c5583ffa21e9f20bcc
Luan Carlos <luancarlos021@gmail.com>
2020-06-05 20:08:17 -0300

    Criação e configuração de rotas

--- src/server.js
+++ src/server.js
@@ -1,5 +1,29 @@
 const express = require("express")
 const server = express()
 
+
+// Configurar pasta pública
+server.use(express.static("public"))
+
+
+
+// Configurar caminhos da aplicação
+
+// Pagina inicial
+// req: Requisição/pedido
+// res: Resposta
+server.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+server.get("/create-point", (req, res) => {
+    res.sendFile(__dirname + "/views/create-point.html")
+})
+
+server.get("/search-results", (req, res) => {
+    res.sendFile(__dirname + "/views/search-results.html")
+})
+
+
 // ligar o servidor
 server.listen(3000)
